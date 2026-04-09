4377345e0708a22b64ec36260131816bf1d61025
Charity Rymbai <charityrymbai@hotmail.com>
2026-04-10 03:39:36 +0530

    change

--- backend/src/redis.ts
+++ backend/src/redis.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379", {
-  maxRetriesPerRequest: 3,
+  maxRetriesPerRequest: null,
   retryStrategy(times) {
     const delay = Math.min(times * 50, 2000);
     return delay;
